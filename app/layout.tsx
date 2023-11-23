bfbdbc28ce5571efeda3c24580314e2dcdf8a319
Pradeep Kaswan <pradeepkaswan7@gmail.com>
2023-11-23 16:11:35 +0530

    feat: add and configured shadcn/ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
+import '../styles/globals.css';
 import React from 'react';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +18,14 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+				>
+					{children}
+				</ThemeProvider>
+			</body>
 		</html>
 	);
 }
