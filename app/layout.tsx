1b8b2515eccef31b80f6d4324e449debbf644fa0
Pradeep Kaswan <pradeepkaswan7@gmail.com>
2023-11-24 16:12:12 +0530

    feat: add and configured clerk for auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,21 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter, Space_Grotesk } from 'next/font/google';
 import '../styles/globals.css';
 import React from 'react';
 import { ThemeProvider } from '@/contexts/theme-provider';
 import { ClerkProvider } from '@clerk/nextjs';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({
+	subsets: ['latin'],
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+	variable: '--font-inter',
+});
+
+const spaceGrotesk = Space_Grotesk({
+	subsets: ['latin'],
+	weight: ['300', '400', '500', '600', '700'],
+	variable: '--font-spaceGrotesk',
+});
 
 export const metadata: Metadata = {
 	title: 'Stack Overflow - Where Developers Learn, Share, & Build Careers',
@@ -19,9 +29,16 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<ClerkProvider>
+		<ClerkProvider
+			appearance={{
+				elements: {
+					formButtonPrimary: 'bg-black',
+					footerActionLink: 'text-black',
+				},
+			}}
+		>
 			<html lang='en'>
-				<body className={inter.className}>
+				<body className={`${inter.variable} ${spaceGrotesk.variable}`}>
 					<ThemeProvider
 						attribute='class'
 						defaultTheme='system'
